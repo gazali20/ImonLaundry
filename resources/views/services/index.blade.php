88284e81a64579fa8d2530ea84af4fac8cfe2dad
gazali20 <gazalirisfan@gmail.com>
2025-03-20 21:05:05 +0700

    dashboard dan lain lain

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -52,7 +52,7 @@
                         prevText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M15 5L9 12L15 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
                         nextText: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4.5 h-4.5 rtl:rotate-180"> <path d="M9 5L15 12L9 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </svg>',
                         labels: {
-                            perPage: "{select}",
+                            perPage: "{select}"
                         },
                         layout: {
                             top: "{search}",
